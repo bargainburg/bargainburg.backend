3f7d7f3057dda81455749104e2af5766d6dc120b
shamboyant <p344816@vt.edu>
2013-09-20 18:34:54 -0400

    Deleted company name from point of contacts as well as added associations

---
+++ api/db/migrate/20130920222503_create_point_of_contacts.rb
@@ -0,0 +1,12 @@
+class CreatePointOfContacts < ActiveRecord::Migration
+  def change
+    create_table :point_of_contacts do |t|
+      t.string :name
+      t.string :phone
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
