e326acd2e4e8da5c624e447231a6c174502eb709
Edna Morales <edna1@vt.edu>
2013-10-03 18:08:10 -0400

    reverted previous changes to sort nested merchants

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
 	has_many :coupons
-	has_many :merchants, :order => 'name ASC' #order the merchants within the category in alphabetical order
+	has_many :merchants
 end
