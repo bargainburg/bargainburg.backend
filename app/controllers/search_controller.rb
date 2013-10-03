b1c67a36205b30683d8676e4992c283fccfd42cd
Tyler Kahn <tkahn6@gmail.com>
2013-10-03 12:49:02 -0400

    don't actually need to search categories

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,7 +8,6 @@ class SearchController < ApplicationController
       query = "%#{params[:query]}%"
       @coupons = Coupon.accessible_by(current_ability).where("description like ? or name like ?", query, query).order('name ASC')
       @merchants = Merchant.accessible_by(current_ability).where("description like ? or name like ?" , query, query).order('name ASC')
-      @categories = Category.accessible_by(current_ability).where("name like ?", query).order('name ASC')
       @vals = @coupons.concat(@merchants).concat(@categories)
       @vals.sort! {|a,b| a.name <=> b.name }
     end
