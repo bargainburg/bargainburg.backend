4d32dfc8518a46c016fbe819f5242a2a0b7ebb87
shamboyant <p344816@vt.edu>
2013-10-03 01:04:07 -0400

    Added category to the search as well as making sure hidden coupons remain hidden and unapproved merchants are hidden

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,7 @@ class SearchController < ApplicationController
 	def index 
 
 		if params[:query].present?
-			@vals = {"coupons" => Coupon.where("description like ? or name like ?", "%#{params[:query]}%", "%#{params[:query]}%"), "merchants" => Merchant.where("description like ? or name like ?" , "%#{params[:query]}%","%#{params[:query]}%"), "categories" => Category.where("name like ?", "%#{params[:query]}%")}
+			@vals = {"coupons" => Coupon.where("(description like ? or name like ?) and hidden = ?", "%#{params[:query]}%", "%#{params[:query]}%", false), "merchants" => Merchant.where("(description like ? or name like ?) and approved = ? " , "%#{params[:query]}%","%#{params[:query]}%", true), "categories" => Category.where("name like ?", "%#{params[:query]}%")}
 		end
 		if (params[:callback].present?)
 			render json: @vals, callback: params[:callback]
