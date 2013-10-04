d3b411e6a1d7ebd808f9147948ee042fd1b6aa7d
Brian Skarda <bskarda@vt.edu>
2013-10-04 01:33:39 -0400

    updates abilities so that merchants can see all of their coupons even if hidden

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,20 +4,19 @@ class Ability
   def initialize(user)
     user ||= User.new
 
-    can :read, Coupon, :hidden => false
-    can :read, Coupon, :end_date.gt => Time.now
-    can :read, Coupon, :begin_date.lt => Time.now
-
     can :read, Category
-
-    can :read, PointOfContact
-
-    can :read, Merchant, :approved => true
 
     if user.persisted?
       can :manage, Merchant, :user_id => user.id
       can :manage, Coupon, :merchant => {:user_id => user.id}
-    end
+    else
+      can :read, PointOfContact
 
+      can :read, Merchant, :approved => true
+      
+      can :read, Coupon, :hidden => false
+      can :read, Coupon, :end_date.gt => Time.now
+      can :read, Coupon, :begin_date.lt => Time.now
+    end
   end
 end
