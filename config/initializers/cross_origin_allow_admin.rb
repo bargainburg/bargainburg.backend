465d5047c902e15da9c84193126288270c87d18b
Tyler Kahn <tkahn6@gmail.com>
2013-10-13 10:50:35 -0400

    cross origin allow credentials

--- config/initializers/cross_origin_allow_admin.rb
+++ config/initializers/cross_origin_allow_admin.rb
@@ -9,6 +9,7 @@ class CrossOriginAllowAdmin
     headers['Access-Control-Allow-Origin'] = 'http://admin.bargainburg.co'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*, x-requested-with, content-type, accept, origin, referer, user-agent'
+    headers['Access-Control-Allow-Credentials'] = 'true'
 
     [status, headers, body]
   end
