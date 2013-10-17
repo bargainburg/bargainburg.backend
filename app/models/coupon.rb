95f41ca5362599b171c8867f264927dc21355d71
shamboyant <p344816@vt.edu>
2013-10-17 00:45:48 -0400

    Coupons now have validations

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -8,7 +8,9 @@ class Coupon < ActiveRecord::Base
 	    :content_type => [ 'image/jpg', 'image/png' ], 
 	    :message => "File must be of type jpg or png"
    validates_attachment_size :image, :less_than => 2.megabytes
+   validates_presence_of [:begin_date, :end_date, :hidden, :name, :category_id, :merchant_id], :on => :create
    validate :validate_end_after_start
+   
    def validate_end_after_start
    	if end_date && begin_date
 		errors.add(:end_date, "End Date before Start Date") if end_date < begin_date
