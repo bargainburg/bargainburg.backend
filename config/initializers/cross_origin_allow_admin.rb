70a7f02656126e3c769604db0cca8cd95f19533a
Brian Skarda <bskarda@vt.edu>
2013-10-30 18:18:49 -0400

    Changes cross origin header to allow dev and base

--- config/initializers/cross_origin_allow_admin.rb
+++ config/initializers/cross_origin_allow_admin.rb
@@ -6,7 +6,7 @@ class CrossOriginAllowAdmin
   def call(env)
     status, headers, body = @app.call(env)
 
-    headers['Access-Control-Allow-Origin'] = '*.bargainburg.co'
+    headers['Access-Control-Allow-Origin'] = 'http://dev.bargainburg.co, http://bargainburg.co'
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, OPTIONS'
     headers['Access-Control-Allow-Headers'] = '*, x-requested-with, content-type, accept, origin, referer, user-agent'
     headers['Access-Control-Allow-Credentials'] = 'true'
