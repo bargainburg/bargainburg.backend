fa84b9ba6124366664357ee79e713f4862d318f5
Cassidy Heath <cassidy2@vt.edu>
2013-11-14 14:05:44 -0500

    finished merging

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,7 +2,7 @@ class Merchant < ActiveRecord::Base
 	has_many :coupons
 	belongs_to :category
 	has_many :PointOfContact
-<<<<<<< HEAD
+	
   belongs_to :user  
   
   validates_presence_of [:name, :approved, :phone, :price_range, :category_id]
@@ -10,7 +10,6 @@ class Merchant < ActiveRecord::Base
   belongs_to :user
 
   validates_presence_of [:name, :phone, :price_range, :category_id]
->>>>>>> 523cb0ab82c03947e333e12737c9393041a0bf61
   
   validates_phone_number :merchant, :phone => /(\(\d{3}\) ?)(\d{3}(-|.))?\d{3}(-|.)\d{4}/
 
