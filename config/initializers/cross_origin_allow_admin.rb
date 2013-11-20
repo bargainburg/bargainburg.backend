7b8158f457fb4f8722caaf05d35ae43ee5d19ee1
Brian Skarda <bskarda@vt.edu>
2013-11-20 18:27:11 -0500

    Adds Access Control Request Method to CORS

--- config/initializers/cross_origin_allow_admin.rb
+++ config/initializers/cross_origin_allow_admin.rb
@@ -12,6 +12,7 @@ class CrossOriginAllowAdmin
       headers['Access-Control-Allow-Origin'] = 'http://dev.bargainburg.co'
     end
     headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, OPTIONS'
+    headers['Access-Control-Request-Method'] = '*'
     headers['Access-Control-Allow-Headers'] = '*, x-requested-with, content-type, accept, origin, referer, user-agent'
     headers['Access-Control-Allow-Credentials'] = 'true'
 
